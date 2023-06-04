5033bba47dd7eef67908d46af761ffb9bae23663
damoglumuratevren <60298354+damoglumuratevren@users.noreply.github.com>
2023-06-04 12:58:31 +0300

    first Commit

---
+++ Med.Core/Med.Core.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace Med.Core.Domain.Entities
+{
+  public class BaseEntity
+  {
+    public string Id { get; set; }
+    public string CreatedBy { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public string? UpdatedBy { get; set; }
+    public DateTime? UpdatedDate { get; set; }
+    public string? DeletedBy { get; set; }
+    public DateTime? DeletedDate { get; set; }
+  }
+}
